510c78a5ec32ef22b4553b86057f820b885dd0eb
Naimur Reza <naimur.rezaa@gmail.com>
2023-03-26 22:24:12 +0600

    Created navbar

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <div className='bg-slate-800 px-20 py-4 flex   justify-between items-center'>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+            <div>
+                <ul className='text-slate-200 flex gap-8  font-medium'>
+                    <li className='hover:text-orange-500'><a href="order">Order</a></li>
+                    <li className='hover:text-orange-500'><a href="">Order Review</a></li>
+                    <li className='hover:text-orange-500'><a href="">Manage Inventory</a></li>
+                    <li className='hover:text-orange-500'><a href="">Login</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
