b1dbcc7b1dcb62b91deda26d4c960b3c9a1cb120
Naimur Reza <naimur.rezaa@gmail.com>
2023-04-06 01:33:31 +0600

    cart sesh

--- src/Layout/ReviewOrder.jsx
+++ src/Layout/ReviewOrder.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useLoaderData , useNavigation} from "react-router-dom";
 import Cart from "../Components/Shop/Cart/Cart";
 import SingleReviewItem from "./SingleReviewItem";
-import { removeFromDb } from "../utilities/fakedb";
+import { deleteShoppingCart, removeFromDb } from "../utilities/fakedb";
 import Loading from "../Components/Loading/Loading";
 
 const ReviewOrder = () => {
@@ -24,8 +24,12 @@ const ReviewOrder = () => {
     setCart(remaining);
     removeFromDb(id)
   };
+  const clearCart = () => {
+    deleteShoppingCart();
+    setCart([]);
+  }
   return (
-    <div className="my-container flex flex-col lg:flex-row justify-around pt-3">
+    <div className="my-container flex flex-col lg:flex-row justify-around pt-3 p-3">
       <div className="product-container">
         {cart.map((it) => (
           <SingleReviewItem item={it} key={it.id} handleRemove={handleRemove} />
@@ -33,7 +37,7 @@ const ReviewOrder = () => {
       </div>
 
       <div className="cart-container ">
-        <Cart cart={cart}>Proceed Checkout</Cart>
+        <Cart cart={cart} clearCart={clearCart} >Proceed Checkout</Cart>
       </div>
     </div>
   );
