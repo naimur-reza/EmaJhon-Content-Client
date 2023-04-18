8ce14883a4af31b90fdc05660eb61d31e4d4b3ce
Anik <naimur.rezaa@gmail.com>
2023-04-18 15:00:07 +0600

    bug fixed

--- src/Components/Shop/Card/SingleData.jsx
+++ src/Components/Shop/Card/SingleData.jsx
@@ -3,15 +3,17 @@ import React from "react";
 const SingleData = (props) => {
   const { ratings, name, price, id, seller, img } = props.data;
   return (
-    <div className="card  lg:w-56 w-40 lg:h-[520px] bg-base-100 shadow-xl">
+    <div className="card  lg:w-56 w-[173px]  lg:h-[440px] bg-base-100 shadow-xl">
       <figure>
         <img className="" src={img} alt="Shoes" />
       </figure>
       <div className="card-body p-3">
         <h2 className="card-title text-sm lg:text-lg">{name}</h2>
-        <p className="font-semibold">Price: ${price}</p>
-        <p className="text-sm lg:text-base">Manufacturer: {seller}</p>
-        <p className="text-sm lg:text-base">Ratting: {ratings}</p>
+        <p className="font-semibold text-xs">
+          Price: $<span className="text-yellow-600">{price}</span>
+        </p>
+        <p className="text-xs lg:text-base">Manufacturer: {seller}</p>
+        <p className="text-xs lg:text-base">Ratting: {ratings}</p>
       </div>
       <button
         onClick={() => props.handleAddToCart(props.data)}
